cf61953a6b3ea37d8cfd45fbfcb5b71d9cb49a07
szn1995 <sujan.koju.sastra@gmail.com>
2021-04-06 09:49:30 +0545

    first commit

---
+++ src/app/employee.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Employee} from "./employee";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  private apiServerUrl = 'http://localhost:8080/employees';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getEmployees(): Observable<Employee[]> {
+    console.log("CALLING GET EMPLOYEES API");
+    return this.http.get<Employee[]>(this.apiServerUrl);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(this.apiServerUrl, employee);
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(this.apiServerUrl, employee);
+  }
+
+  public deleteEmployee(id: number): Observable<void> {
+    return this.http.delete<void>(this.apiServerUrl + '/' + id);
+  }
+
+  public getEmployee(id: number): Observable<Employee> {
+    return this.http.get<Employee>(this.apiServerUrl + '/' + id);
+  }
+}
